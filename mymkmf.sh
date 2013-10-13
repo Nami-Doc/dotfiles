97d5943fe034c7a786e512b4426c95478b9004ef
Nami-Doc <vendethiel@hotmail.fr>
2013-10-13 14:40:13 +0200

    lazyness

---
+++ mymkmf.sh
@@ -0,0 +1,35 @@
+name=$1
+
+if [ $# -eq 2 ] ; then
+	$libs = "-L. -l$2"
+fi
+
+echo "CC = gcc
+
+SRC = $(wildcard *.c) $(wildcard */*.c)
+
+OBJ = $(SRC:.c=.o)
+
+FLAGS = -W -Wall -Werror -pedantic -ansi -combine
+
+LIBS = $libs
+
+NAME = $name
+
+all: $(NAME)
+
+$(NAME): $(OBJ)
+	$(CC) -o $(NAME) $(OBJ) $(LIBS) $(FLAGS)
+
+clean:
+	rm $(OBJ)
+
+fclean: clean
+	rm $(NAME)
+
+re: fclean all
+
+test: all
+	...
+
+.PHONY: clean fclean test" > Makefile
